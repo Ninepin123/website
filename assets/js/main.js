8e3f01f7115c4f81d62d6e21fa8e5768804535da
HentaiBigUncle <142314294+HentaiBigUncle@users.noreply.github.com>
2024-12-26 00:48:28 +0800

    small change on index
    small change on index and a big problem on modal, I need to fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,5 +182,11 @@
         }, index * 500); // Stagger the fade effect by 500ms for each paragraph
     });
 });
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
 
 })();
